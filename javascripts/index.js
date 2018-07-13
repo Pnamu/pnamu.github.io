a311fc1aec8d41de74333b30907323c220e90597
Pnamu <limepnamu@gmail.com>
2018-07-13 16:59:34 +0900

    Update index.js

--- javascripts/index.js
+++ javascripts/index.js
@@ -1,7 +1,7 @@
 window.onload = () => {
   let images = document.querySelectorAll('#created-by img')
   document.getElementById('created-by').addEventListener('mouseover', (e) => {
-    images[0].setAttribute('src', '/images/i.png')
+    images[0].setAttribute('src', '/images/images/GitHub-Mark-120px-plus.png')
     images[1].setAttribute('src', '/images/sigong.png')
   })
   document.getElementById('created-by').addEventListener('mouseout', (e) => {
