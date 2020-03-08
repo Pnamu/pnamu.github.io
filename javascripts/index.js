64a65187f7fadb5aae0fee73aa5edf264f2ca337
Kyujin Cho <thy2134@gmail.com>
2020-03-08 17:49:53 +0900

    Update to Next.js version

--- javascripts/index.js
+++ javascripts/index.js
@@ -1,9 +1,11 @@
 window.onload = () => {
   let images = document.querySelectorAll('#created-by img')
   document.getElementById('created-by').addEventListener('mouseover', (e) => {
+    images[0].setAttribute('src', '/images/i.png')
     images[1].setAttribute('src', '/images/sigong.png')
   })
   document.getElementById('created-by').addEventListener('mouseout', (e) => {
+    images[0].setAttribute('src', '/images/GitHub-Mark-120px-plus.png')
     images[1].setAttribute('src', '/images/tw_icon.png')
   })
 }
