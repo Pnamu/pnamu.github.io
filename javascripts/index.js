7ea615c6cf73075738517feea1919607d2621886
Pnamu <limepnamu@gmail.com>
2023-11-04 11:20:03 +0900

    Update index.js

--- javascripts/index.js
+++ javascripts/index.js
@@ -1,26 +1,30 @@
-function setDisplayAppreance(self){
-    var body = document.querySelector('body'); 
-    var btn = document.getElementById('btnDisplayMode'); 
-    if(self.value === '다크모드로 전환'){  //다크모드로 전환 버튼을 누른 경우
-      body.style.backgroundColor = 'black';  //배경색 변경
-      body.style.color = 'white'; //css가 적용되지 않은 기본 텍스트 색상 변경
-      btn.style.backgroundColor = "yellow"; //버튼의 배경색 변경
-      btn.style.color = "black"; //버튼의 텍스트 색상 변경
-      self.value = '라이트모드로 전환';
-    } else { //라이트모드로 전환 버튼을 누른 경우
-      body.style.backgroundColor = 'white';
-      body.style.color = 'black';
-      btn.style.backgroundColor = "black";
-      btn.style.color = "white";
-      self.value = '다크모드로 전환';
 window.onload = () => {
-  let images = document.querySelectorAll('#created-by img')
+  let images = document.querySelectorAll('#created-by img');
   document.getElementById('created-by').addEventListener('mouseover', (e) => {
-    images[0].setAttribute('src', '/images/i.png')
-    images[1].setAttribute('src', '/images/sigong.png')
-  })
+    images[0].setAttribute('src', '/images/i.png');
+    images[1].setAttribute('src', '/images/sigong.png');
+  });
   document.getElementById('created-by').addEventListener('mouseout', (e) => {
-    images[0].setAttribute('src', '/images/GitHub-Mark-120px-plus.png')
-    images[1].setAttribute('src', '/images/tw_icon.png')
-  })
+    images[0].setAttribute('src', '/images/GitHub-Mark-120px-plus.png');
+    images[1].setAttribute('src', '/images/tw_icon.png');
+  });
+}
+
+function setDisplayAppreance(self) {
+  var body = document.querySelector('body');
+  var btn = document.getElementById('btnDisplayMode');
+  
+  if (self.value === '다크모드로 전환') {
+    body.style.backgroundColor = 'black';
+    body.style.color = 'white';
+    btn.style.backgroundColor = 'yellow';
+    btn.style.color = 'black';
+    self.value = '라이트모드로 전환';
+  } else {
+    body.style.backgroundColor = 'white';
+    body.style.color = 'black';
+    btn.style.backgroundColor = 'black';
+    btn.style.color = 'white';
+    self.value = '다크모드로 전환';
+  }
 }
